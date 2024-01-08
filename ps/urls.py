3091ca8cd41c2112cef0c0d124f40f74dffed09a
sudhakarjha0042 <jhas0042@gmail.com>
2024-01-08 20:32:30 +0530

    Django Structure Created

---
+++ ps/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
