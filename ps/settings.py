6cf165108cf2738455cd597cc439a17a1fb08460
sudhakarjha0042 <jhas0042@gmail.com>
2024-01-10 22:38:38 +0530

    Added django admin static css and js

--- ps/settings.py
+++ ps/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sr6!5@2@kl%s$2obpq$vzah&gsccmh%i=zcuiyoxh!u_hjn*r1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.build.sh','now.sh','localhost']
+ALLOWED_HOSTS = ['.vercel.app','.build.sh','now.sh','localhost','127.0.0.1']
 
 # Application definition
 
