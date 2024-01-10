debb5081c4e46b8940f49498783925ef02d64234
sudhakarjha0042 <jhas0042@gmail.com>
2024-01-10 21:58:53 +0530

    deploy22

--- ps/settings.py
+++ ps/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sr6!5@2@kl%s$2obpq$vzah&gsccmh%i=zcuiyoxh!u_hjn*r1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.build.sh']
+ALLOWED_HOSTS = ['.vercel.app','.build.sh','now.sh','localhost']
 
 # Application definition
 
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'ps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fC3-ga-3Ce64GfdbdAfGA4bDFdGefg33',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '58405',
     }
 }
 
@@ -119,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'construction', 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'construction', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
