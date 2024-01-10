6ec07f8e2be3e1c3a4b50847a2bbb4f50f4a7fd5
vishav-pathania <vishavpathania40@gmail.com>
2024-01-10 20:58:14 +0530

    intergrating contact us form with frontend

--- construction/forms.py
+++ construction/forms.py
@@ -6,3 +6,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'category', 'message']
+
+    category = forms.ChoiceField(choices=[
+        ('Residential', 'Residential'),
+        ('Industrial', 'Industrial'),
+        ('Commercial', 'Commercial'),
+    ])
