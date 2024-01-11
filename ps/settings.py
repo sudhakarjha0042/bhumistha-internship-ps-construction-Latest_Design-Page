c558fa9cfdd1f196d4af3d35ee4946b4d07f0b11
sudhakarjha0042 <jhas0042@gmail.com>
2024-01-11 21:16:33 +0530

    vercel deployyysjkk

--- ps/settings.py
+++ ps/settings.py
@@ -123,8 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'construction', 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'construction', 'staticfiles_build')
-print(STATIC_ROOT)
+
 
 
 
