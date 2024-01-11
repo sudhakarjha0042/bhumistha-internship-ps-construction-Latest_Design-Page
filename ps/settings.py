131129156280f6d3c6e1c15c89962a22841d0823
sudhakarjha0042 <jhas0042@gmail.com>
2024-01-11 21:42:34 +0530

    vercel deployyysjkdd

--- ps/settings.py
+++ ps/settings.py
@@ -118,7 +118,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, images, fonts)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'construction', 'static'),
 ]
